d78c2caa66331a688eac6c560e60a24a3470602f
Jaimy <j.t.s.kwa@students.uu.nl>
2016-10-05 16:38:45 +0200

    Added skeleton classes

---
+++ Tetris/Tetris/Block.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+class Block
+{
+    Vector2 sprite;
+    Vector2 Position;
+    Keys key;
+
+    public Block()
+    {
+
+    }
+
+    public void Update(GameTime gameTime)
+    {
+
+    }
+
+    public void Draw(GameTime gameTime)
+    {
+
+    }
+
+    public void Die()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+    }
+}
