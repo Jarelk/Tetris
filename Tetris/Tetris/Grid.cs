3aa5a9277ab495c5893705b2e31273df48a3d341
Jari van Wijk <Jari van Wijk@LAPTOP>
2016-10-05 16:19:25 +0200

    grid class gemaakt

--- Tetris/Tetris/Grid.cs
+++ Tetris/Tetris/Grid.cs
@@ -3,7 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
 
 class Grid
 {
+
+    public Grid()
+        {
+
+        }
+
+
+    protected void Update(GameTime gameTime)
+        {
+
+        }
+
+    protected void Draw(GameTime gameTime)
+        {
+
+        }
 }
